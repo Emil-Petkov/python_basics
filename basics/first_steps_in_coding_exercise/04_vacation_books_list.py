b34f0050c4c560b3b9b672b08daa91292eeec8ae
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2022-09-21 14:35:23 +0300

    delete

--- basics/first_steps_in_coding_exercise/04_vacation_books_list.py
+++
@@ -1,7 +0,0 @@
-count_pages_per_book = int(input())
-read_pages_per_hour = int(input())
-count_days = int(input())
-
-total = (count_pages_per_book // read_pages_per_hour) // count_days
-
-print(total)
--- basics/first_steps_in_coding_lab/08_pet_shop.py
+++
@@ -1,9 +0,0 @@
-count_food_packaging_per_dogs = int(input())
-count_food_packaging_per_cats = int(input())
-
-price_food_per_dogs = count_food_packaging_per_dogs * 2.50
-price_food_per_cats = count_food_packaging_per_cats * 4.00
-
-total = price_food_per_dogs + price_food_per_cats
-
-print(f'{total} lv.')
