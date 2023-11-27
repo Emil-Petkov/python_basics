bf6afdc7a1a5a3a2356e31fbbe723d82e2ce4bc2
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-11-27 12:09:58 +0200

    Create 07_projects_creation.py

---
+++ basics/01_first_steps/01_lab/07_projects_creation.py
@@ -0,0 +1,8 @@
+architect_name = input()
+num_of_projects = int(input())
+
+hours_per_one_project = 3
+
+needed_hours = num_of_projects * hours_per_one_project
+
+print(f'The architect {architect_name} will need {needed_hours} hours to complete {num_of_projects} project/s.')
