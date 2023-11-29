17c6da62e6739a710af22cd1c06d982f894ca4bd
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-11-29 15:56:51 +0200

    Create 09_fruit_or_vegetable.py

---
+++ basics/03_conditional_statements_advanced/01_lab/09_fruit_or_vegetable.py
@@ -0,0 +1,10 @@
+fruits_or_vegetables = input()
+
+if fruits_or_vegetables in ['banana', 'apple', 'kiwi', 'cherry', 'lemon', 'grapes', ]:
+    print('fruit')
+
+elif fruits_or_vegetables in ['tomato', 'cucumber', 'pepper', 'carrot']:
+    print('vegetable')
+
+else:
+    print('unknown')
