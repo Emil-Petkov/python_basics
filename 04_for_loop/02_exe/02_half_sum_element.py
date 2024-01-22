11148a0a29c02ea274e96443c90be599375e0b5b
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-23 01:08:24 +0200

    Create 02_half_sum_element.py

---
+++ 04_for_loop/02_exe/02_half_sum_element.py
@@ -0,0 +1,23 @@
+import sys
+
+n_numbers = int(input())
+
+max_numbers = -sys.maxsize
+sum_of_numbers = 0
+
+for num in range(n_numbers):
+    current_number = int(input())
+
+    sum_of_numbers += current_number
+
+    if current_number > max_numbers:
+        max_numbers = current_number
+
+sum = sum_of_numbers - max_numbers
+
+if sum == max_numbers:
+    print(f'Yes\nSum = {sum}')
+
+else:
+    diff = abs(sum - max_numbers)
+    print(f'No\nDiff = {diff}')
