b1e6536caea8c1c6234a4d8c0e7d07bd8db1536a
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-23 18:26:24 +0200

    Create 03_sum_numbers.py

---
+++ 05_while_loop/01_lab/03_sum_numbers.py
@@ -0,0 +1,9 @@
+check_number = int(input())
+
+sum_of_numbers = 0
+
+while not sum_of_numbers >= check_number:
+    current_number = int(input())
+    sum_of_numbers += current_number
+
+print(sum_of_numbers)
