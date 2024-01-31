69fe16d7f86ec85520b0c2fe6abb3aa7f9582fee
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-31 11:37:07 +0200

    Create 04_sum_of_two_numbers.py

---
+++ 06_nested_loops/01_lab/04_sum_of_two_numbers.py
@@ -0,0 +1,16 @@
+start = int(input())
+end = int(input())
+magic_number = int(input())
+
+counter = 0
+
+for i in range(start, end + 1):
+    for j in range(start, end + 1):
+
+        counter += 1
+
+        if i + j == magic_number:
+            print(f'Combination N:{counter} ({i} + {j} = {magic_number})')
+            exit()
+
+print(f'{counter} combinations - neither equals {magic_number}')
