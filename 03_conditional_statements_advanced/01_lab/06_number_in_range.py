47c08efd44f24e3980395d8bc0eff5a149d36663
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-17 18:47:30 +0300

    .

--- 03_conditional_statements_advanced/01_lab/06_number_in_range.py
+++
@@ -1,9 +0,0 @@
-def number_in_range(num: int):
-    if -100 <= num <= 100 and not num == 0:
-        return 'Yes'
-    else:
-        return 'No'
-
-
-number = int(input())
-print(number_in_range(number))
