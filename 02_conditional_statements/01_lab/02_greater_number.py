c8f302f7e091851ccd3e64880d75bed4f0487ba6
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-18 11:25:47 +0300

    Create 02_greater_number.py

---
+++ 02_conditional_statements/01_lab/02_greater_number.py
@@ -0,0 +1,7 @@
+def greater_number(f_number: int, s_number: int):
+    return max(f_number, s_number)
+
+
+first_number = int(input())
+second_number = int(input())
+print(greater_number(first_number, second_number))
