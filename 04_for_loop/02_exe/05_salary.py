a0704d626a0ac4e043560ee334575dc09cb6e3db
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-24 20:10:53 +0300

    Create 05_salary.py

---
+++ 04_for_loop/02_exe/05_salary.py
@@ -0,0 +1,20 @@
+n_open_tabs = int(input())
+salary = int(input())
+
+fines = {
+    'Facebook': 150,
+    'Instagram': 100,
+    'Reddit': 50,
+}
+
+for tab in range(n_open_tabs):
+    current_tab = input()
+
+    if current_tab in fines:
+        salary -= fines[current_tab]
+
+    if salary <= 0:
+        print('You have lost your salary.')
+        exit()
+
+print(salary)
