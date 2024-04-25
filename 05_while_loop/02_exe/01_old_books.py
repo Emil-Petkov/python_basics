d4bb5e2a926ad2203d8ef3537deec15ad275ecea
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-25 09:11:07 +0300

    Create 01_old_books.py

---
+++ 05_while_loop/02_exe/01_old_books.py
@@ -0,0 +1,16 @@
+the_book = input()
+command = input()
+
+counter_books = 0
+
+while not command == 'No More Books':
+    if command == the_book:
+        print(f'You checked {counter_books} books and found it.')
+        exit()
+
+    else:
+        counter_books += 1
+
+    command = input()
+
+print(f'The book you search is not here!\nYou checked {counter_books} books.')
