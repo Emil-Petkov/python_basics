ca1207e634867ea630ead2282f95a6e27e6ce9dc
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-29 21:03:37 +0300

    Create 03_vacation.py

---
+++ 05_while_loop/02_exe/03_vacation.py
@@ -0,0 +1,28 @@
+trip_price = float(input())
+current_money = float(input())
+counter_days = 0
+consecutive_spend_days = 0
+
+while current_money < trip_price and consecutive_spend_days < 5:
+    action = input()
+    amount = float(input())
+    counter_days += 1
+
+    if action == "spend":
+        current_money -= amount
+
+        if current_money < 0:
+            current_money = 0
+        consecutive_spend_days += 1
+
+    elif action == "save":
+        current_money += amount
+        consecutive_spend_days = 0
+
+    if current_money >= trip_price:
+        print(f"You saved the money for {counter_days} days.")
+        break
+
+    elif consecutive_spend_days == 5:
+        print("You can't save the money.")
+        print(counter_days)
