68fc423f4d80b85ee2e5b98d1e536de9144111a6
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-04 14:59:14 +0300

    Create 05_coins.py

---
+++ 05_while_loop/02_exe/05_coins.py
@@ -0,0 +1,17 @@
+money = float(input())
+
+coins = [2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01]
+
+counter = 0
+
+while money > 0:
+    for coin in coins:
+
+        if money >= coin:
+            count = int(money // coin)
+
+            counter += count
+            money -= count * coin
+            money = round(money, 2)
+
+print(counter)
